f4fc9e24667bf4c06a35a80d44f8e5f0f63ca237
Geroshabu <oonstks@gmail.com>
2017-05-20 09:36:55 +0900

    Add a handle_open code instead using iex

---
+++ sources/Chapter5/handle_open.exs
@@ -0,0 +1,10 @@
+handle_open = fn
+  {:ok, file} -> "First line: #{IO.read(file, :line)}"
+  {_, error} -> "Error: #{:file.format_error(error)}"
+end
+
+# 存在するファイルを開く
+IO.puts handle_open.(File.open("handle_open.exs"))
+# そして, 存在しないファイルを開こうとする
+IO.puts handle_open.(File.open("nonexistent"))
+
